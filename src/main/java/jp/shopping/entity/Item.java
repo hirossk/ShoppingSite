851122dd2f9d4cf0436dbed54c72b58cca7a7ab5
Yoshida Jobi <Gakuen@yoshida.com>
2023-07-19 05:57:12 +0900

    Git挑戦中

--- src/main/java/jp/shopping/entity/Item.java
+++ src/main/java/jp/shopping/entity/Item.java
@@ -18,6 +18,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Item implements Serializable{
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "code")
