13f2c4e3d63266d3db575752b10ce03499037470
Hiroyuki Sasaki <sasaki@yoshida-g.ac.jp>
2023-07-20 14:19:42 +0900

    微調整

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,13 +11,7 @@ CREATE MEMORY TABLE "PUBLIC"."ITEM"(
 );            
 ALTER TABLE "PUBLIC"."ITEM" ADD CONSTRAINT "PUBLIC"."CONSTRAINT_2" PRIMARY KEY("CODE");       
 -- 6 +/- SELECT COUNT(*) FROM PUBLIC.ITEM;    
-INSERT INTO "PUBLIC"."ITEM" VALUES
-(1, 'ギター', 30000, 'guitar'),
-(2, 'ミディキーボード', 48000, 'midi'),
-(3, 'エレクトーン', 300000, 'electone'),
-(4, 'サックス', 400000, 'sax'),
-(5, 'トランペット', 250000, 'trumpet'),
-(6, 'ホルン', 500000, 'horn');
+
 DROP TABLE IF EXISTS cart;               
 CREATE MEMORY TABLE "PUBLIC"."CART"(
     "CODE" INTEGER NOT NULL,
@@ -25,9 +19,3 @@ CREATE MEMORY TABLE "PUBLIC"."CART"(
 );          
 ALTER TABLE "PUBLIC"."CART" ADD CONSTRAINT "PUBLIC"."CONSTRAINT_1" PRIMARY KEY("CODE");       
 -- 5 +/- SELECT COUNT(*) FROM PUBLIC.CART;    
-INSERT INTO "PUBLIC"."CART" VALUES
-(1, 1),
-(2, 1),
-(3, 1),
-(4, 4),
-(6, 2);               
