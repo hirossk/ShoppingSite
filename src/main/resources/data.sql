f545c259e0742358231c6fecfaeff56b8498f122
hirossk <70559038+hirossk@users.noreply.github.com>
2023-07-21 10:02:34 +0900

    ベータ６（data.sqlコメント修正）

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 DELETE FROM "PUBLIC"."CART";
 DELETE FROM "PUBLIC"."ITEM";
 
---ここから修正してもらいます。
+--ここから修正します。
 --INSERT INTO "PUBLIC"."ITEM" VALUES
 --(1, 'ギター', 30000, 'guitar'),
 --(2, 'ミディキーボード', 48000, 'midi'),
