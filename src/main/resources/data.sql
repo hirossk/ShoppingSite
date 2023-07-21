b760fd58794d9e32829e999969ca5d5c5a9ea376
hirossk <70559038+hirossk@users.noreply.github.com>
2023-07-21 09:10:19 +0900

    ベータ３（SQLのコメントアウト）

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,16 @@
-INSERT INTO "PUBLIC"."ITEM" VALUES
-(1, 'ギター', 30000, 'guitar'),
-(2, 'ミディキーボード', 48000, 'midi'),
-(3, 'エレクトーン', 300000, 'electone'),
-(4, 'サックス', 400000, 'sax'),
-(5, 'トランペット', 250000, 'trumpet'),
-(6, 'ホルン', 500000, 'horn');
+DELETE FROM "PUBLIC"."CART";
+DELETE FROM "PUBLIC"."ITEM";
+--INSERT INTO "PUBLIC"."ITEM" VALUES
+--(1, 'ギター', 30000, 'guitar'),
+--(2, 'ミディキーボード', 48000, 'midi'),
+--(3, 'エレクトーン', 300000, 'electone'),
+--(4, 'サックス', 400000, 'sax'),
+--(5, 'トランペット', 250000, 'trumpet'),
+--(6, 'ホルン', 500000, 'horn');
    
-//INSERT INTO "PUBLIC"."CART" VALUES
-//(1, 1),
-//(2, 1),
-//(3, 1),
-//(4, 4),
-//(6, 2);               
+--INSERT INTO "PUBLIC"."CART" VALUES
+--(1, 1),
+--(2, 1),
+--(3, 1),
+--(4, 4),
+--(6, 2);                     
