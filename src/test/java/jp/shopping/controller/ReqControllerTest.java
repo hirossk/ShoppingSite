ced63a5e842d71529622a9893490ed22e24f7653
Hiroyuki Sasaki <sasaki@yoshida-g.ac.jp>
2024-03-17 08:26:20 +0900

    Minor bug fix list.html

---
+++ src/test/java/jp/shopping/controller/ReqControllerTest.java
@@ -0,0 +1,51 @@
+package jp.shopping.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReqControllerTest {
+
+	@Test
+	void testCartIn() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testCartDel() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testShowView() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testDisplayTable() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testDisplayItemList() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testDisplayShopInfo() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testDisplayCartInfo() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testDisplayShopping() {
+		fail("まだ実装されていません");
+	}
+
+}
